8c1bad10b4998a92f27a298df8dfd5ffca31e094
Skrubstar <wangzilongcharles@gmail.com>
2025-10-28 17:37:59 -0700

    Website from Local

---
+++ vite-project/src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Intro() {
+    return <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
+        <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Charles Wang</h1>
+        <p className='text-base md:text-xl mb-3 font-medium'>Data Engineer</p>
+        <p className='text-sm max-w-xl mb-6'
+        >
+        Recent Data Science Graduate from the University of California San Diego. 
+        Passionate for all things to do with data and all the models and visualizations derived from it. 
+        Feel free to check out my PROJECTS or CONTACT me if you'd like to chat. 
+        <br />
+        Outside of programming, you'll often find me tinkering with computers, reading, or trying to bake something new. 
+
+        </p>
+
+
+    </div>
+}
+
+export default Intro;
---
+++ vite-project/src/components/data/portfolio.js
@@ -0,0 +1,22 @@
+export default [
+{
+   title: 'Agricultural Forecasting',
+   imgUrl: '/assets/crop_yield.png',
+   stack: ['Python', 'HTML', 'Scikit-Learn'],
+   link: 'https://global-crop-yield-climate-simulator.streamlit.app/',
+
+},
+{
+   title: 'BirdCLEF',
+   imgUrl: '/assets/birdclef.png',
+   stack: ['Python', 'Torch', 'Neural Networks'],
+   link: 'https://github.com/outsider1113/mlc-sp25-aquafina',
+
+},
+{
+   title: 'San Diego Parking',
+   imgUrl: '/assets/sd_parking.png',
+   stack: ['HTML', 'Javascript', 'Neural Python'],
+   link: 'https://ch-lum.me/sd-parking/',
+},
+]
